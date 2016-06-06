cc76941b2da7d45fe40a0362638e99cd4f86fc23
Thomas M. DuBuisson <tommd@galois.com>
2016-06-06 10:50:59 -0700

    Helpful comments in the demo.hs

--- examples/Demo.hs
+++ examples/Demo.hs
@@ -5,19 +5,19 @@ import Vision.Detector.Edge (canny)
 main :: IO ()
 main =
   mellow blackRGBA                      -- Black image to start
-         (\d _ -> return (imgWords d))  -- Noop conversion for straight video
+         (\d _ -> return (getEdges d))  -- Custom image manipulator
          return                         -- Noop renders
          (defaultEventHandler return)
               -- The default event handler allows us to quit using 'esc'
               -- as well as save screen captures with 's'.
 
-imgWords :: RGBA -> RGBA
-imgWords i0 =
-  let g,bl,ot,ed :: Grey
-      g  = convert i0
-      bl = blur 2 g
-      ed = canny 2 0 0 bl -- No, I don't know good canny constants.
-  in convert ed
+getEdges :: RGBA -> RGBA
+getEdges i0 =
+  let g,bl,ed :: Grey
+      g  = convert i0        -- Obtain the Grey image from the RGBA
+      bl = blur 3 g          -- Blur to get rid of noise
+      ed = canny 2 64 512 bl -- Canny edge detection with some reasonable constants
+  in convert ed              -- Convert to RGBA for rendering
 
 blackRGBA :: RGBA
 blackRGBA = I.fromFunction (Z :. 480 :. 640) (const (RGBAPixel 0 0 0 0))
