d41e18ffa4875378dc34d8ddf61b2951859a0c3e
Thomas M. DuBuisson <thomas.dubuisson@gmail.com>
2023-05-19 22:05:44 -0700

    Use ffmpeg 4 not 5

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/603e89eef935b22754e1406364ba0921997c71c4.tar.gz";
     sha256 = "sha256:0b3wmjc5q7mv1y0gkg2m8b6kr5vakakkcrn022cpmdqlk7f097xx";
   }) {};
-in new.mkShell {
+in git.mkShell {
   buildInputs = with git; [
     ghc
     haskellPackages.haskell-language-server
@@ -11,7 +11,7 @@ in new.mkShell {
     zlib.dev
     libGL.dev
     libGLU.dev
-    (ffmpeg-headless.override { buildAvutil = true; })
+    (ffmpeg_4-headless.override { buildAvutil = true; })
     pkg-config
   ];
 }
